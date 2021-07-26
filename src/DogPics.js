3f560804d2f078670b0dd6ef657ac78739845694
ellijs <ellijs@hotmail.com>
2021-07-26 17:30:35 -0400

    Completed

--- src/DogPics.js
+++ src/DogPics.js
@@ -1,27 +1,27 @@
 import React, { useState, useEffect } from "react";
 
+
 // WARNING: this useEffect will run in an infinite loop!
 // to fix, pass an empty array as the second argument for useEffect
 function DogPics() {
   const [images, setImages] = useState([]);
-
+  console.log(images)
   useEffect(() => {
     console.log("useEffect");
     fetch("https://dog.ceo/api/breeds/image/random/3")
       .then((r) => r.json())
       .then((data) => {
-        console.log("setState");
-        setImages(data.messages);
+        console.log(data);
+        setImages(data.message);
       });
-  });
+  },[]);
 
-  console.log("render");
+  console.log(images);
+  const display = images.map((image) => <img src={image} key={image} />)
 
   return (
     <div>
-      {images.map((image) => (
-        <img src={image} key={image} />
-      ))}
+      {display}
     </div>
   );
 }
